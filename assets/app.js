1e2f98162da5ddcca5df2a58499fc2b8bbfccb68
Miguel Rivas Perez <mrivasperez1996@gmail.com>
2020-01-18 21:40:39 -0800

    added comments to app.js

--- assets/app.js
+++ assets/app.js
@@ -11,16 +11,17 @@ $(".toggleButton").hover(
 });
 */
 
-//on click remove or add .active
-
+//update the output panel (iframe)
 function updateOutput(){
+    //we will manipulate the iframe to load css, and html
     $("iframe").contents().find("html").html("<html><head><style type = 'text/css'>" + $("#cssPanel").val() + "</style></head><body>" + $("#htmlPanel").val() + "</body></html>");
-
+    //make the javascript code run on the iframe
     document.getElementById("outputPanel").contentWindow.eval($("#javascriptPanel").val());
 
     
 }
 
+//on click remove or add .active
 $(".toggleButton").click(function(){
     //add or remove active class on click
     $(this).toggleClass("active");
@@ -37,11 +38,12 @@ $(".toggleButton").click(function(){
 
 
 
-
+//resize the panels to be heigh of window
 $(".panel").height($(window).height() - $("#header").height() - 21);
+//resite the panels to be width of window
 $(".panel").width($(window).width() / 2 - 10);
 
-
+//make the output automatically update as user types in textarea(s)
 $("textarea").on('change keyup paste', function() {
     updateOutput()
 });
